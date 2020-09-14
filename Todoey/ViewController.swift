b105dc28f201038624e6125fa81d85bf24d6d641
Prakash Sundesha <prakashmali9@gmail.com>
2020-09-15 00:01:05 +0530

    Initial Commit

--- Todoey/ViewController.swift
+++ Todoey/ViewController.swift
@@ -8,13 +8,59 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ToDoListViewController: UITableViewController {
+    
+    var defaults = UserDefaults.standard
+    
+    var itemArray = [ "1", "2", "3"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let items = defaults.array(forKey: "ToDoListArray") as? [String]{
+            itemArray = items
+        }
     }
-
-
+    
+    // Mark - TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:   "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row ]
+        return cell
+    }
+    
+    // MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if  tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK - AddButton Action
+    
+    @IBAction func addButtonPressed(_ sender: Any) {
+        
+        var textfield = UITextField()
+        let alert = UIAlertController(title: "Add item to your List", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add item.", style: .default) { (action) in
+            self.itemArray.append(textfield.text ?? "")
+            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            textfield = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
 }
 
